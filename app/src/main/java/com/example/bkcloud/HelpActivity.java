dea8e6e8f03abcb83f446a834ed1e97a3ba08f97
Khanhhcmut <lephukhanhhuynh@gmail.com>
2025-12-10 04:04:04 +0700

    Commit

--- app/src/main/java/com/example/bkcloud/HelpActivity.java
+++ app/src/main/java/com/example/bkcloud/HelpActivity.java
@@ -2,20 +2,32 @@ package com.example.bkcloud;
 
 import android.os.Bundle;
 
-import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
 
 public class HelpActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
-        setContentView(R.layout.activity_help);
 
+        boolean openManual = getIntent().getBooleanExtra("open_manual", false);
 
+        if (openManual) {
+            setContentView(R.layout.activity_help);
+
+            findViewById(R.id.btnExitManual).setOnClickListener(v -> finish());
+        }
+        else {
+            setContentView(R.layout.help_menu);
+
+            findViewById(R.id.btnHelp1).setOnClickListener(v -> {
+                getIntent().putExtra("open_manual", true);
+                recreate();
+            });
+
+            findViewById(R.id.btnHelp2).setOnClickListener(v -> {});
+            findViewById(R.id.btnHelp3).setOnClickListener(v -> {});
+            findViewById(R.id.btnExitHelp).setOnClickListener(v -> finish());
+        }
     }
 }
